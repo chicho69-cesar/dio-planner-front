a99c1d38b6cf29d0136d914a8df80de30bf0adc4
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-04-08 10:34:21 -0600

    :art: Add skeleton event list loading

---
+++ src/components/home/LoadingEvents.jsx
@@ -0,0 +1,28 @@
+import { Box, FlatList, HStack, Skeleton } from "native-base";
+
+export default function LoadingEvents() {
+  const skeletons = [ 1, 2, 3, 4 ];
+
+  return <FlatList
+    data={skeletons}
+    ItemSeparatorComponent={<></>}
+    keyExtractor={(item) => `${item}`}
+    showsVerticalScrollIndicator={false}
+    renderItem={({ item }) => (
+      <Box my={4} rounded='lg' borderWidth={1} key={item}>
+        <HStack justifyContent='flex-start' space={2}>
+          <Skeleton 
+            h="200" w="150" borderLeftRadius='lg' 
+            startColor="coolGray.600" 
+          />
+
+          <Box w='56%' p={2} h='100%' borderRightRadius='lg'>
+            <Skeleton startColor="coolGray.600" rounded='md' mb={2}/>
+            <Skeleton h="5" rounded="full" startColor="amber.400" mb={8}/>
+            <Skeleton.Text startColor='coolGray.600'/>
+          </Box>
+        </HStack>
+      </Box>
+    )}
+  />;
+}
