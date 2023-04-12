c03144912e51fe6a153cad61b43e6b60789ebba8
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-04-12 07:37:47 -0600

    :sparkles: Add FABCreate component

---
+++ src/components/FABCreate.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Fab, Icon } from 'native-base'
+import { AntDesign } from '@expo/vector-icons'
+
+export function FABCreate({ navigation, route }) {
+  return (
+    <Fab
+      renderInPortal={false}
+      shadow={2}
+      right={5}
+      bottom={70}
+      size="16"
+      colorScheme="amber"
+      bg="amber.400"
+      icon={
+        <Icon
+          as={<AntDesign name="plus" />}
+          size={8}
+          fontWeight="bold"
+          color="black"
+        />
+      }
+      onPress={() => {}}
+    />
+  )
+}
