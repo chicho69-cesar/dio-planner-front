e8f960cc5da95163ff46b2ede50e133e0ddbb006
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-07-16 23:30:19 -0600

    :sparkleS: Migrate utils and validations

---
+++ src/utils/currency-formatter.js
@@ -0,0 +1,8 @@
+export const getCurrencyFormat = (money) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(parseFloat(money))
+}
